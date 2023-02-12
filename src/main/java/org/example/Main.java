9ea2cfcfd9b5bd9af6e559ba9720de452b6e63a6
SergeyBazhenov <8618@mail.ru>
2023-02-12 23:33:03 +0500

    Create binary print

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,25 @@
-package org.example;public class Main {
+package org.example;
+import java.net.SocketOption;
+import java.util.Random;
+
+
+public class Main {
+    public static int k = 2000;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       // k = 2000;
+        int i = new Random().nextInt(k); //это кидалка случайных чисел!)
+        System.out.println(i);
+        printBinary(i);
+        System.out.println();
+        System.out.println(Integer.toBinaryString(i));
+    }
+    private static void printBinary(int val) {
+        if (val < 2){
+            System.out.print(val);
+            return;
+        }
+        int digit = val % 2;
+        printBinary(val / 2);
+        System.out.print(digit);
     }
 }
